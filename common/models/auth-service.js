310bc4ba201beee16f0006c06684f5e079c5fb60
yunas.pamatda <yunas.pamatda@dataon.com>
2019-12-19 17:49:23 +0700

    add method checkPhoneNumber and checkEmail

--- common/models/auth-service.js
+++ common/models/auth-service.js
@@ -1,15 +1,15 @@
 "use strict";
 
 module.exports = function(AuthService) {
+  // ================================================================
+  //            checkPhoneNumber
+  // ================================================================
   AuthService.remoteMethod("checkPhoneNumber", {
     accepts: [
       {
-        arg: "params",
+        arg: "phoneNumber",
         type: "Object",
-        required: true,
-        http: {
-          source: "body"
-        }
+        required: true
       }
     ],
     returns: {
@@ -23,9 +23,79 @@ module.exports = function(AuthService) {
     }
   });
 
-  AuthService.checkPhoneNumber = async params => {
+  AuthService.checkPhoneNumber = async phoneNumber => {
     try {
-      return true;
+      if (!phoneNumber || typeof phoneNumber != "number") {
+        throw new Error("params incomplete or not a number");
+      }
+      const User = app.models.User;
+      const valid = await new Promise(resolve => {
+        User.find(
+          {
+            where: { phoneNumber: phoneNumber },
+            fields: ["id"]
+          },
+          (err, res) => {
+            if (err) {
+              throw err;
+            } else {
+              resolve(res.length > 0 ? true : false);
+            }
+          }
+        );
+      });
+
+      return { valid: valid };
+    } catch (e) {
+      throw e.message;
+    }
+  };
+
+  // ================================================================
+  //            checkEmail
+  // ================================================================
+  AuthService.remoteMethod("checkEmail", {
+    accepts: [
+      {
+        arg: "email",
+        type: "string",
+        required: true
+      }
+    ],
+    returns: {
+      arg: "data",
+      type: "object",
+      root: true
+    },
+    http: {
+      path: "/checkEmail",
+      verb: "get"
+    }
+  });
+
+  AuthService.checkEmail = async email => {
+    try {
+      if (!email || typeof phoneNumber != "string") {
+        throw new Error("params incomplete or not a string");
+      }
+      const User = app.models.User;
+      const valid = await new Promise(resolve => {
+        User.find(
+          {
+            where: { email: email },
+            fields: ["id"]
+          },
+          (err, res) => {
+            if (err) {
+              throw err;
+            } else {
+              resolve(res.length > 0 ? true : false);
+            }
+          }
+        );
+      });
+
+      return { valid: valid };
     } catch (e) {
       throw e.message;
     }
